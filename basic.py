91ca34dd785966f539295f74c5f934338d41f472
bibek.171544 <bibek.171544@ncit.edu.np>
2022-05-02 18:29:26 +0545

    Commited via cli

---
+++ basic.py
@@ -0,0 +1,15 @@
+# # a = 20
+# # print(a)
+# # a = a+a
+# # print(a)
+# # import sys
+# # print(sys.version)
+# bibek = "hello bibek"
+# # print(bibek[6])
+# print("hello \n world")
+# print(len(bibek))
+def f1():
+    x = 100
+    print(x)
+f1()    
+type(x)
